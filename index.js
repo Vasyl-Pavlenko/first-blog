a1463b7931613fcd76c9e99395295ff918730d1d
Vasyl Pavlenko <genlus@ukr.net>
2023-08-23 19:41:57 +0300

    cors was changed

--- index.js
+++ index.js
@@ -45,6 +45,12 @@ const upload = multer({ storage });
 
 app.use(express.json());
 app.use(cors({ origin: '*' }));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationsErrors, UserController.login);
